b1846e54023ca03d4b60b990caf24f8e85170d5c
Jan Ambrožič <jan3594@gmail.com>
2013-04-01 03:58:46 +0200

    Added first polygons on the map.
    Fixed the region data and added first 3 regions to the map.

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
+Slovenia Regions Data
+================================
+
 Sources for this project: 
+
 http://nominatim.openstreetmap.org/
 http://leafletjs.com/
+
+You can find live demo at: 
+http://slovenia-regions-map.herokuapp.com/
--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([46.151241, 14.995463], 9);
 
-		L.tileLayer('http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png', {
+		L.tileLayer('http://{s}.tile.cloudmade.com/ee24b382df8040c4867a605bdc6c54a2/997/256/{z}/{x}/{y}.png', {
 			maxZoom: 18,
 			attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
 		}).addTo(map);
 
+		var circle = L.circle([46.151241, 14.995463], 500, {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5
+}).addTo(map);
 
+L.geoJson(sloveniaRegionsData[0].geojson).addTo(map);
+L.geoJson(sloveniaRegionsData[1].geojson).addTo(map);
+//gorenjska
+L.geoJson(sloveniaRegionsData[2].geojson).addTo(map);
